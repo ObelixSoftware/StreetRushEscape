d0f8c3fa8d7bd8ce7fd19667dd63995b4691a285
Lennie De Villiers <lennie.work@gmail.com>
2025-07-10 15:46:36 +0200

    Explotion/sound
    Importing of the visuals and sound - cleanup of code

---
+++ Assets/Scripts/ExplotionEffect.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ExplotionEffect : MonoBehaviour
+{
+    public AudioClip explosionSound;
+
+    void Start()
+    {
+        AudioSource.PlayClipAtPoint(explosionSound, transform.position);
+        Destroy(gameObject, 1f); // Remove explosion after 1 second
+    }
+}
