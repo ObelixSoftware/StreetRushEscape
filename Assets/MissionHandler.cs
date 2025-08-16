1c74d34c218802a18099644bf1b0936045891d6b
Inkythunder <98986057+Inkythunder@users.noreply.github.com>
2025-08-16 16:58:30 +0100

    Initial mission structure and window

---
+++ Assets/MissionHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MissionHandler : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text _title;
+
+    [SerializeField]
+    private TMP_Text _waypointDescription;
+
+    private int activeMission = 0;
+    private int activeWaypoint = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject missionList = GameObject.Find("Missions");
+        GameObject missionObj = missionList.transform.GetChild(activeMission).gameObject;
+
+        Mission mission = missionObj.GetComponent<Mission>();
+
+        GameObject waypointObj = missionObj.transform.GetChild(activeWaypoint).gameObject;
+        Waypoint waypoint = waypointObj.GetComponent<Waypoint>();
+
+        _title.text = mission.missionName;
+        _waypointDescription.text = waypoint.waypointDescription;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
