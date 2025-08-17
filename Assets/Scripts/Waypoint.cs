d17077b9edb4140d8344acc796472001742c26ce
Ol-Vinny <42969794+Ol-Vinny@users.noreply.github.com>
2025-08-17 17:12:06 -0600

    Added Dialogue and Hideaways
    Dialogue logic was added to waypoints
    
    Hideways made into a prefab and added to the saferoom buildings
    
    The mission and waypoint scripts somehow ended up in the /assets folder instead of the /assets/scripts folder, also moved those and (hopefully) fixed a bug that was making the waypoint collider not work.

---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    //Fires the provided Dialogue when the player
+    public Dialogue dialogue;
+    public string waypointDescription;
+}
