5581e278420face2ef539ab937a8547f876eaa1c
Inkythunder <98986057+Inkythunder@users.noreply.github.com>
2025-08-17 16:40:33 +0100

    Active mission changes on waypoint reached

--- Assets/MissionHandler.cs
+++ Assets/MissionHandler.cs
@@ -13,25 +13,56 @@ public class MissionHandler : MonoBehaviour
 
     private int activeMission = 0;
     private int activeWaypoint = 0;
+    private GameObject waypointCollider;
+    private GameObject missionList;
+    private GameObject missionObj;
+    private GameObject waypointObj;
     
     // Start is called before the first frame update
     void Start()
     {
-        GameObject missionList = GameObject.Find("Missions");
-        GameObject missionObj = missionList.transform.GetChild(activeMission).gameObject;
+        missionList = GameObject.Find("Missions");
+        waypointCollider = GameObject.Find("MissionWaypointCollider");
+        activateMissionWaypoint(0, 0);
+    }
+
+    public void onPlayerReachedWaypoint()
+    {
+        Debug.Log("waypoint reached");
+        //If the player has completed the final waypoint in the mission, move to next mission.
+        if(activeWaypoint == missionObj.transform.childCount - 1)
+        {
+            //If the player has also completed the last mission, reset the mission counter.
+            if(activeMission == missionList.transform.childCount - 1)
+            {
+                activateMissionWaypoint(0, 0);
+            } 
+            else
+            {
+                activateMissionWaypoint(activeMission + 1, 0);
+            }
+        } 
+        else 
+        {   
+            activateMissionWaypoint(activeMission, activeWaypoint + 1);
+        }
+    }
+
+    //Changes the active waypoint and mission to next in the list and moves the collider.
+    void activateMissionWaypoint(int missionID, int waypointID)
+    {
+        activeMission = missionID;
+        activeWaypoint = waypointID;
+        missionObj = missionList.transform.GetChild(missionID).gameObject;
 
         Mission mission = missionObj.GetComponent<Mission>();
 
-        GameObject waypointObj = missionObj.transform.GetChild(activeWaypoint).gameObject;
+        waypointObj = missionObj.transform.GetChild(waypointID).gameObject;
         Waypoint waypoint = waypointObj.GetComponent<Waypoint>();
 
         _title.text = mission.missionName;
         _waypointDescription.text = waypoint.waypointDescription;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        waypointCollider.transform.position = waypointObj.transform.position;
     }
 }
